fb9fd6dcd03c0975a6dd80e0f88a794b8bd32002
Anders Ekman <oakismen@gmail.com>
2019-07-02 16:57:45 +0200

    add search for foods, plus styling

--- src/app.js
+++ src/app.js
@@ -1,10 +1,95 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import graphql from './helpers/graphql';
 
 class App extends React.Component {
+
+    style = {
+        wrapper: {
+            display: 'flex',
+            flex: 1,
+            flexDirection: 'column',
+            fontFamily: 'Product Sans Regular',
+            width: '80%',
+            margin: 'auto',
+        },
+        title: {
+            fontFamily: 'Product Sans Black Regular',
+            fontSize: '2.5em',
+            fontWeight: 'bold',
+            textAlign: 'center',
+        },
+        form: {
+            display: 'flex',
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+        },
+        input: {
+            flex: 1,
+            padding: 15,
+            fontSize: '1.2em',
+            border: 'solid 1px #ccc',
+            borderRadius: 5,
+            boxShadow: '1px 1px 8px 0px #ccc',
+        },
+        button: {
+            padding: '15px 45px',
+            marginLeft: 8,
+            fontSize: '1.2em',
+            border: 'solid 1px #ccc',
+            borderRadius: 5,
+            boxShadow: '1px 1px 8px 0px #ccc',
+            backgroundColor: 'steelblue',
+            color: '#eee'
+        },
+    }
+
+    state = {
+        searchInput: '',
+        searchResults: [],
+    }
+
+    foodByName = async (event) => {
+        event.preventDefault();
+        const search = await graphql(`
+            query FindFood {
+                foodByName(name: "${this.state.searchInput}") {
+                    name
+                    number
+                    weight
+                    group
+                }
+            }
+        `);
+        const searchResults = search.data.data.foodByName;
+        console.log(`Found ${searchResults.length} foods.`, searchResults);
+        this.setState({
+            searchResults,
+        });
+    }
+
+    onInputChange = (event) => this.setState({ searchInput: event.target.value });
+
     render() {
         return (
-            <h3>React JSX</h3>
+            <div style={this.style.wrapper}>
+                <h3 style={this.style.title}>GraphQL calorie calculator</h3>
+                <br />
+                <form style={this.style.form}>
+                    <input
+                        style={this.style.input}
+                        onChange={this.onInputChange}
+                        value={this.state.searchInput}
+                    />
+                    <button onClick={this.foodByName} style={this.style.button}>Sök</button>
+                </form>
+                <br />
+                <ul>
+                    {this.state.searchResults.map(food => (
+                        <li key={food.number}>{food.name}</li>
+                    ))}
+                </ul>
+            </div>
         );
     }
 };
---
+++ src/helpers/graphql.js
@@ -0,0 +1,15 @@
+export default (query) => fetch(
+    'http://localhost:3000/graphql',
+    {
+        method: 'post',
+        headers: {
+            'Accept':'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            query,
+        })
+    })
+    .then(json => json.json())
+    .then(data => ({ data }))
+    .catch(console.log);
